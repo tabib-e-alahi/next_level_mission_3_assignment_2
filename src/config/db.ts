a5d2d3a1964f8f01e7c63ba120e08a0eb33282eb
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-07 23:45:56 +0600

    Update database schema to include vehicle and booking relationships

--- src/config/db.ts
+++ src/config/db.ts
@@ -18,20 +18,21 @@ const initDB = async () => {
         `);
 
   await pool.query(`
-            CREATE TABLE IF NOT EXISTS Vehicles(
-            id SERIAL PRIMARY KEY,
-            vehicle_name VARCHAR(200) NOT NULL,
-            type VARCHAR(10) NOT NULL,
-            registration_number VARCHAR(100) UNIQUE NOT NULL,
-            daily_rent_price INT NOT NULL,
-            availability_status VARCHAR(20) NOT NULL
-            )
+        CREATE TABLE IF NOT EXISTS Vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(200) NOT NULL,
+        type VARCHAR(10) NOT NULL,
+        registration_number VARCHAR(100) UNIQUE NOT NULL,
+        daily_rent_price INT NOT NULL,
+        availability_status VARCHAR(20) NOT NULL
+        )
             `);
 
   await pool.query(`
     CREATE TABLE IF NOT EXISTS Bookings(
     id SERIAL PRIMARY KEY,
-    customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+    customer_id INT REFERENCES Users(id) ON DELETE CASCADE,
+    vehicle_id INT REFERENCES Vehicles(id) ON DELETE CASCADE,
     )
         `);
 };
