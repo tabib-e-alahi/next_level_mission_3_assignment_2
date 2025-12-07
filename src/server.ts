a471e22eba5ccd432d93b4bf977fac56481842f4
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-07 18:53:52 +0600

    Add server listening event

--- src/server.ts
+++ src/server.ts
@@ -6,3 +6,7 @@ const port = 5000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running, Hello From Tabib E Alahi..");
 });
+
+app.listen((port) => {
+  console.log(`Server is running on ${port}`);
+});
