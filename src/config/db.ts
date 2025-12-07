d88cecfc6c39e0c29fe25f699becde29827cd16e
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-07 23:33:56 +0600

    Add database setup and schema initialization

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,38 @@
-import {Pool} from 'pg'
-import config from './config'
+import { Pool } from "pg";
+import config from "./config";
 
 const pool = new Pool({
-    connectionString: config.connection_string
-})
+  connectionString: config.connection_string,
+});
+
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role VARCHAR(50) NOT NULL,
+        phoneNo VARCHAR(15),
+        age INT,
+        address TEXT,
+        create_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
+};
+
+export default initDB;
