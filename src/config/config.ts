88f43770f192d0d3454478167b8f1b6c49fb745c
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-07 23:07:56 +0600

    Update dotenv config path to .env file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,8 @@
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({path: path.join(process.cwd(), )})
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const config ={
+    connection_string: process.env.
+}
