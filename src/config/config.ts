19efdd386dae13bf77da69831836fc1d8cf17c3f
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-07 23:08:56 +0600

    Add environment variables to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-const config ={
-    connection_string: process.env.
-}
+const config = {
+  connection_string: process.env.CONNECTION_STRING,
+  port: process.env.PORT,
+};
