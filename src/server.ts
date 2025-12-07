b31e23ca142203b6d7d5738bcddb25515fca52bb
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-07 23:58:56 +0600

    Add database initialization to server setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
+import initDB from "./config/db";
 
 const app = express();
 const port = 5000;
 
 //?parser
 app.use(express.json());
+
+//? db
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running, Hello From Tabib E Alahi..");
