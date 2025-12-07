984bf99a0025c73cd9332359438e098e91aae179
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-07 18:52:52 +0600

    Update server.ts to include Express type imports and endpoint response

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) =>{
-    res.send
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running, Hello From Tabib E Alahi..");
+});
