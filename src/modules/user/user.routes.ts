792fab8ce465d184949bf45753d1d1e5f6a3198b
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 21:22:34 +0600

    Add error handling to user signup route

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -4,10 +4,13 @@ const router = Router();
 
 router.post("/signup", async (req: Request, res: Response) => {
   const { name, email, password, phone, role } = req.body;
-  try{
-
-  }catch(err: any){
-    
+  try {
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: "Error description",
+      errors: "Error description",
+    });
   }
 });
 
