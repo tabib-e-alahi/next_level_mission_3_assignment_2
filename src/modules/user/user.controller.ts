8a15b491772b8c29d6b0f57bc6c9b0ba1f758339
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 21:34:34 +0600

    Refactor user registration logic to use user services

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,10 @@
 import { Request, Response } from "express";
 import { pool } from "../../config/db";
+import { userServices } from "./user.services";
 
 const registerUser = async (req: Request, res: Response) => {
-  const { name, email, password, phone, role } = req.body;
   try {
-    const result = await pool.query(
-      `
-        INSERT INTO Users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *
-        `,
-      [name, email, password, phone, role]
-    );
+    const result = await userServices.
 
     res.status(201).json({
       success: true,
--- src/modules/user/user.services.ts
+++ src/modules/user/user.services.ts
@@ -1,14 +1,15 @@
 import { pool } from "../../config/db";
 
-const registerUser = async(payload: Record<string, unknown>) =>{
-    const result = await pool.query(
-          `
+const registerUser = async (payload: Record<string, unknown>) => {
+  const { name, email, password, phone, role } = payload;
+  const result = await pool.query(
+    `
             INSERT INTO Users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *
             `,
-          [name, email, password, phone, role]
-        );
-}
+    [name, email, password, phone, role]
+  );
 
-export const userServices = {
-    
-}
+  return result;
+};
+
+export const userServices = {registerUser};
