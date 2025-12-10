36e15fb20e149008599269294e9069620b347db6
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 21:24:34 +0600

    Add database query for user signup

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,10 +1,14 @@
 import { Request, Response, Router } from "express";
+import { pool } from "../../config/db";
 
 const router = Router();
 
 router.post("/signup", async (req: Request, res: Response) => {
   const { name, email, password, phone, role } = req.body;
   try {
+    const result = await pool.query(`
+        
+        `)
   } catch (err: any) {
     res.status(500).json({
       success: false,
