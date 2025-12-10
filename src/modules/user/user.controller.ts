b298f72770b4091507a42eae0823368acf5b01e1
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 22:13:34 +0600

    Fix user registration response formatting

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,17 +5,19 @@ const registerUser = async (req: Request, res: Response) => {
   try {
     const result = await userServices.registerUser(req.body);
 
-    if(!result.success){
-        res.status(400).json({
-            success: false,
-            message: re
-        })
+    if (!result.success) {
+      res.status(400).json({
+        success: false,
+        message: result.message,
+      });
+
+      return;
     }
 
     res.status(201).json({
       success: true,
       message: "User registered successfully",
-      data: result,
+      data: result.data,
     });
   } catch (err: any) {
     res.status(500).json({
