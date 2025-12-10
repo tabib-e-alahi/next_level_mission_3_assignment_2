fa9f38f82fdae95d1ff92bcce2bcb3211ce6fbba
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 21:31:34 +0600

    Add user registration route

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,8 +1,9 @@
 import { Request, Response, Router } from "express";
 import { pool } from "../../config/db";
+import { userController } from "./user.controller";
 
 const router = Router();
 
-router.post("/signup", );
+router.post("/signup", userController.registerUser);
 
 export const userRoutes = router;
