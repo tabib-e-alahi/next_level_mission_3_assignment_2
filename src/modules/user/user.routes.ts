d9c4898fdc60c0f983b3a628a22f87a927e674ba
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 21:29:34 +0600

    Remove unused signup endpoint and controller method

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -3,34 +3,6 @@ import { pool } from "../../config/db";
 
 const router = Router();
 
-router.post("/signup", async (req: Request, res: Response) => {
-  const { name, email, password, phone, role } = req.body;
-  try {
-    const result = await pool.query(
-      `
-        INSERT INTO Users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *
-        `,
-      [name, email, password, phone, role]
-    );
-
-    res.status(201).json({
-      success: true,
-      message: "User registered successfully",
-      data: {
-        id: 1,
-        name: "John Doe",
-        email: "john.doe@example.com",
-        phone: "01712345678",
-        role: "customer",
-      },
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: "User registration failed!",
-      errors: err.message,
-    });
-  }
-});
+router.post("/signup", );
 
 export const userRoutes = router;
