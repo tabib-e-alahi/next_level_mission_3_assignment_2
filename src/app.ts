d3bb29591902e63048a5fd5396becebbc4f8a4f5
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 21:15:34 +0600

    Remove unused signup API endpoint

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,5 @@ initDB();
 
 app.use("/api/v1/auth", userRoutes);
 
-app.post("/api/v1/auth/signup", async (req: Request, res: Response) => {
-    const {name, email, password, phone, role} = req.body;
-});
 
 export default app;
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,11 +1,9 @@
-import { Router } from "express"
+import { Request, Response, Router } from "express";
 
 const router = Router();
 
-router.post("/signup", )
+router.post("/signup", async (req: Request, res: Response) => {
+  const { name, email, password, phone, role } = req.body;
+});
 
-
-
-export const userRoutes = {
-
-}
+export const userRoutes = {};
