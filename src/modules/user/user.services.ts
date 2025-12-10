63909fe8a608dde3598982bbdde3a45654562893
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 22:04:34 +0600

    Remove password length validation

--- src/modules/user/user.services.ts
+++ src/modules/user/user.services.ts
@@ -5,7 +5,7 @@ const registerUser = async (payload: Record<string, unknown>) => {
   const { name, email, password, phone, role } = payload;
 
   if((password as string).length < 6){
-    return err
+    return 
   }
 
   const hashedPassword = await bcrypt.hash(password as string, 10);
