761562aee9f3dc5933f4ce4cb98ff269066c6780
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-10 21:27:34 +0600

    Fix SQL query injection vulnerability in user signup route

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -6,9 +6,12 @@ const router = Router();
 router.post("/signup", async (req: Request, res: Response) => {
   const { name, email, password, phone, role } = req.body;
   try {
-    const result = await pool.query(`
+    const result = await pool.query(
+      `
         INSERT INTO Users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *
-        `)
+        `,
+      [name, email, password, phone, role]
+    );
   } catch (err: any) {
     res.status(500).json({
       success: false,
