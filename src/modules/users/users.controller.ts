7b1f1476a09a78af9f0f828d677f5d27254fb2a2
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 00:10:48 +0600

    Add async getAllUsers function to users controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,11 @@
+import { Request, Response } from "express"
 
-const getAllUsers = () =>{
-
+const getAllUsers = async(req: Request, res: Response) =>{
+    try {
+        
+    } catch (err: any) {
+        
+    }
 }
 
 export const userController = {
