7abcc3968e080845544ca38d3f12b6ee54f1ce24
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-11 22:38:47 +0600

    Update auth middleware to validate token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,3 +1,11 @@
-const auth = () =>{
-    return async(req: Request, res: Res)
-}
+import { Request, Response } from "express";
+
+const auth = () => {
+  return async (req: Request, res: Response) => {
+    const token = req.headers.authorization;
+
+    if(!token){
+        throw new Error()
+    }
+  };
+};
