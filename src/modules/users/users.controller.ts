b670b35f13407bb45d96914a12fa40430e95fd70
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 00:25:48 +0600

    Add success response to getAllUsers API

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,6 +8,10 @@ const getAllUsers = async (req: Request, res: Response) => {
     if(result.rows.length === 0){
         throw new Error("Users data not found.")
     }
+    res.status(200).json({
+        success: true,
+        message: Users retrieved successfully
+    })
   } catch (err: any) {
     return res.status(500).json({
       success: false,
