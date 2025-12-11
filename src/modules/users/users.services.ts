9534654bd6ee3a3d6d542a33f351781e51e9de49
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 00:09:48 +0600

    Refactor user service imports and query result handling

--- src/modules/users/users.services.ts
+++ src/modules/users/users.services.ts
@@ -1,13 +1,13 @@
-import { pool } from "../../config/db"
+import { pool } from "../../config/db";
 
-const getAllUsers = async() =>{
-
-    const result = await pool.query(`
+const getAllUsers = async () => {
+  const result = await pool.query(`
         SELECT id, name, email, phone, role FROM Users
-        `)
+    `);
 
-}
+  return result;
+};
 
 export const userServices = {
-    getAllUsers
-}
+  getAllUsers,
+};
