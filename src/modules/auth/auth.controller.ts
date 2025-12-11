722a729a8b6970703264f2dbefb5b6f85221a384
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-11 15:15:44 +0600

    Improve auth controller error handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -21,7 +21,21 @@ const registerUser = async (req: Request, res: Response) => {
 
 const signInUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-  const result = await authServices.signInUser(email, password);
+  try {
+    const result = await authServices.signInUser(email, password);
+
+    return res.status(201).json({
+      success: true,
+      message: "User sign in suce",
+      data: result,
+    });
+  } catch (err: any) {
+    return res.status(500).json({
+      success: false,
+      message: "User registration failed!",
+      errors: err.message,
+    });
+  }
 };
 
 export const authController = { registerUser };
