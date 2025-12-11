cc0b34e854fa64f01ff6d0e9f1c2aeb43fd90718
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-11 22:39:47 +0600

    Add JWT verification to auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import config from "../config/config";
 
 const auth = () => {
   return async (req: Request, res: Response) => {
     const token = req.headers.authorization;
 
     if(!token){
-        throw new Error()
+        throw new Error("You are not authorized!");
     }
+
+    const decoded = jwt.verify(token, config.jwt_secret)
   };
 };
