37fe276b580fe343ee56896bbc665e65e8097a90
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 00:26:48 +0600

    Update users controller to return data on successful retrieval

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -5,18 +5,19 @@ const getAllUsers = async (req: Request, res: Response) => {
   try {
     const result = await userServices.getAllUsers();
 
-    if(result.rows.length === 0){
-        throw new Error("Users data not found.")
+    if (result.rows.length === 0) {
+      throw new Error("Users data not found.");
     }
     res.status(200).json({
-        success: true,
-        message: Users retrieved successfully
-    })
+      success: true,
+      message: "Users retrieved successfully",
+      data: result.rows,
+    });
   } catch (err: any) {
     return res.status(500).json({
       success: false,
       message: err.message,
-      data: []
+      data: [],
     });
   }
 };
