09e9db65782dac8358ecd53886b7e577cf1f76c0
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 00:23:48 +0600

    Fix getAllUsers API endpoint to return empty result when no users exist

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,7 +3,9 @@ import { userServices } from "./users.services";
 
 const getAllUsers = async (req: Request, res: Response) => {
   try {
-    const result = await userServices.getAllUsers;
+    const result = await userServices.getAllUsers();
+
+    if(result.rows.length === 0)
   } catch (err: any) {
     return res.status(500).json({
       success: false,
