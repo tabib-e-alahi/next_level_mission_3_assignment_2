330d1501b2313a68d08799e8ca5166769e61de47
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-11 22:42:47 +0600

    Update auth middleware to include next function

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config/config";
 
 const auth = () => {
-  return async (req: Request, res: Response) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
 
     if(!token){
