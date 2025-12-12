6d6ce862f5f6dd7feb550f5dcd47bd5de81b0cdd
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 12:52:57 +0600

    Update vehicle controller to return created vehicle data

--- src/modules/vehicles/vehicles.controller.ts
+++ src/modules/vehicles/vehicles.controller.ts
@@ -5,11 +5,16 @@ const createVehicle = async (req: Request, res: Response) => {
   try {
     const result = await vehicleServices.createVehicle(req.body);
     return res.status(201).json({
-        success: true,
-        message: 
-    })
+      success: true,
+      message: "Vehicle created successfully",
+      data: result.rows[0],
+    });
   } catch (error: any) {
-    console.log("From controller: ", error.message);
+    return res.status(500).json({
+      success: true,
+      message: "Vehicle created successfully",
+      data: result.rows[0],
+    })
   }
 };
 
