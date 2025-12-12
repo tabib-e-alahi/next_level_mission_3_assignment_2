5820f29292dea26151bebde9af447951d1e72d07
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 11:19:56 +0600

    Refactor vehicle service validation

--- src/modules/vehicles/vehicles.services.ts
+++ src/modules/vehicles/vehicles.services.ts
@@ -1,13 +1,20 @@
 const getAllVehicles = async (payload: Record<string, unknown>) => {
-    try {
-        const {vehicle_name, type, registration_number, daily_rent_price, availability_status} = payload;
-        if(!['car', 'bike', 'van', 'SUV'].includes(type as string)){
-            throw new Error("Invalid vehicle types! Types: 'car', 'bike', 'van', or 'SUV'")
-        }
-        
-    } catch (error) {
-        
+  try {
+    const {
+      vehicle_name,
+      type,
+      registration_number,
+      daily_rent_price,
+      availability_status,
+    } = payload;
+    //! checking car types
+    if (!["car", "bike", "van", "SUV"].includes(type as string)) {
+      throw new Error(
+        "Invalid vehicle types! Types: 'car', 'bike', 'van', or 'SUV'"
+      );
     }
+    //checking
+  } catch (error) {}
 };
 
 export const vehicleServices = {
