c121f4c8a5a90b3639b7d1f20e007fb7f84bbc66
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 11:16:56 +0600

    Add type validation to getAllVehicles

--- src/modules/vehicles/vehicles.services.ts
+++ src/modules/vehicles/vehicles.services.ts
@@ -1,7 +1,9 @@
 const getAllVehicles = async (payload: Record<string, unknown>) => {
     try {
         const {vehicle_name, type, registration_number, daily_rent_price, availability_status} = payload;
-
+        if(['car', 'bike', 'van', 'SUV'].includes(type)){
+            
+        }
         
     } catch (error) {
         
