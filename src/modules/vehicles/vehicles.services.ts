1c41a1c1051440aaddb14a71518cbd7ea2b1b8e4
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 11:46:56 +0600

    Update vehicle insertion query to return inserted data

--- src/modules/vehicles/vehicles.services.ts
+++ src/modules/vehicles/vehicles.services.ts
@@ -35,8 +35,8 @@ const getAllVehicles = async (payload: Record<string, unknown>) => {
 
     const result = await pool.query(
         `
-        INSERT INTO Vehicles(vehicle_name, type, registration_number, daily_rent_price, availability_status) VALUES($1, $2, $3, $4, $5) RE
-        `
+        INSERT INTO Vehicles(vehicle_name, type, registration_number, daily_rent_price, availability_status) VALUES($1, $2, $3, $4, $5) RETURNING *
+        `, [vehicle_name, type, registration_number, daily_rent_price, availability_status]
     )
     
   } catch (error) {}
