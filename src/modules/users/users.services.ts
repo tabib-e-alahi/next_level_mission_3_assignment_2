72ac2cc004a087c0521297a4e34a489832231560
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 23:35:17 +0600

    Refactor updateUser to validate vehicle data

--- src/modules/users/users.services.ts
+++ src/modules/users/users.services.ts
@@ -9,10 +9,35 @@ const getAllUsers = async () => {
 };
 
 const updateUser = async(payload: Record<string, unknown>) =>{
-  const {userId} = payload;
+  const {
+    vehicleId,
+    vehicle_name,
+    type,
+    registration_number,
+    daily_rent_price,
+    availability_status,
+  } = payload;
 
-  
-}
+  if (type && !["car", "bike", "van", "SUV"].includes(type as string)) {
+    throw new Error(
+      "Invalid vehicle types! Types: 'car', 'bike', 'van', or 'SUV'"
+    );
+  }
+
+  if (daily_rent_price && (daily_rent_price as number) <= 0) {
+    throw new Error(`Daily rent price must be positive: ${daily_rent_price}.`);
+  }
+
+  if (
+    availability_status &&
+    !["available", "booked"].includes(
+      (availability_status as string).toLowerCase()
+    )
+  ) {
+    throw new Error(
+      "Invalid inputs! Availability Status is either 'available' or 'booked'"
+    );
+  }
 
 export const userServices = {
   getAllUsers,
