766a158c6ab889dd61a3f51fc93afa7777149c0a
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 10:55:56 +0600

    Add try-catch block to getAllVehicles

--- src/modules/vehicles/vehicles.controller.ts
+++ src/modules/vehicles/vehicles.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express"
 
 const getAllVehicles = async(req: Request, res: Response) =>{
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 export const vehicleController = {
