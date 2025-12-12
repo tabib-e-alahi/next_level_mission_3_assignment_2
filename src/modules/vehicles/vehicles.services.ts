866a3d0e1ca3652e3943cbc1edcee3d3f073d670
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 11:50:56 +0600

    Refactor vehicle service validation and insertion

--- src/modules/vehicles/vehicles.services.ts
+++ src/modules/vehicles/vehicles.services.ts
@@ -1,56 +1,49 @@
 import { pool } from "../../config/db";
 
 const getAllVehicles = async (payload: Record<string, unknown>) => {
-  try {
-    const {
+  const {
+    vehicle_name,
+    type,
+    registration_number,
+    daily_rent_price,
+    availability_status,
+  } = payload;
+
+  //! checking car types
+  if (!["car", "bike", "van", "SUV"].includes(type as string)) {
+    throw new Error(
+      "Invalid vehicle types! Types: 'car', 'bike', 'van', or 'SUV'"
+    );
+  }
+  //! checking daily rent price is positive or not
+  if (!((daily_rent_price as number) > 0)) {
+    throw new Error(`Daily rent price must be positive: ${daily_rent_price}.`);
+  }
+
+  //! checking availability status
+  const availability_status_lower: string = (
+    availability_status as string
+  ).toLowerCase();
+  if (!["available", "booked"].includes(availability_status_lower)) {
+    throw new Error(
+      "Invalid inputs! Availability Status is either 'available' or 'booked'"
+    );
+  }
+
+  const result = await pool.query(
+    `
+        INSERT INTO Vehicles(vehicle_name, type, registration_number, daily_rent_price, availability_status) VALUES($1, $2, $3, $4, $5) RETURNING *
+        `,
+    [
       vehicle_name,
       type,
       registration_number,
       daily_rent_price,
-      availability_status,
-    } = payload;
+      availability_status_lower,
+    ]
+  );
 
-    //! checking car types
-    if (!["car", "bike", "van", "SUV"].includes(type as string)) {
-      throw new Error(
-        "Invalid vehicle types! Types: 'car', 'bike', 'van', or 'SUV'"
-      );
-    }
-    //! checking daily rent price is positive or not
-    if (!((daily_rent_price as number) > 0)) {
-      throw new Error(
-        `Daily rent price must be positive: ${daily_rent_price}.`
-      );
-    }
-
-    //! checking availability status
-    const availability_status_lower: string = (
-      availability_status as string
-    ).toLowerCase();
-    if (!["available", "booked"].includes(availability_status_lower)) {
-      throw new Error(
-        "Invalid inputs! Availability Status is either 'available' or 'booked'"
-      );
-    }
-
-    const result = await pool.query(
-      `
-        INSERT INTO Vehicles(vehicle_name, type, registration_number, daily_rent_price, availability_status) VALUES($1, $2, $3, $4, $5) RETURNING *
-        `,
-      [
-        vehicle_name,
-        type,
-        registration_number,
-        daily_rent_price,
-        availability_status_lower,
-      ]
-    );
-
-    return result;
-
-  } catch (err: any) {
-    return res.status
-  }
+  return result;
 };
 
 export const vehicleServices = {
