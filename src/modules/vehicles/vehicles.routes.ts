6d0c8f9cd2f1150a35ea14bbc5ff8ab95ac97d4e
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 10:53:56 +0600

    Add auth middleware to vehicle routes

--- src/modules/vehicles/vehicles.routes.ts
+++ src/modules/vehicles/vehicles.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import auth from "../../middleware/auth";
 const router = Router();
 
 //create a vehicle
+router.post("/", auth("admin"), vehi)
 
 export const vehicleRoutes = router;
