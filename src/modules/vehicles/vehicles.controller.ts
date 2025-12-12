0c4122a55d0d0ec2a9ff602e1548f465488fbecb
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-12 12:41:57 +0600

    Fix console.log error handling

--- src/modules/vehicles/vehicles.controller.ts
+++ src/modules/vehicles/vehicles.controller.ts
@@ -2,11 +2,12 @@ import { Request, Response } from "express"
 import { vehicleServices } from "./vehicles.services"
 
 const getAllVehicles = async(req: Request, res: Response) =>{
+
     try {
        const result =  await vehicleServices.getAllVehicles(req.body);
        console.log(result);
     } catch (error:any) {
-        console.log(error.message);
+        console.log(""error.message);
     }
 }
 
