348cc0fa1fdfe07b516ebee7334eab6c03254b17
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 22:06:17 +0600

    Update bookings service query for customer role

--- src/modules/bookings/bookings.services.ts
+++ src/modules/bookings/bookings.services.ts
@@ -57,7 +57,7 @@ const getAllBookings = async (payload: Record<string, unknown>) => {
 
     return result;
   } else if (userRole === "customer") {
-    const book_result = await pool.query
+    const book_result = await pool.query(`SELECT status FROM Bookings `)
     const result = await pool.query(
       `SELECT * FROM Bookings WHERE customer_id=$1`,
       [loggedInUserId]
