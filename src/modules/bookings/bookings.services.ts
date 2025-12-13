42d8c924b87daeea548a8a487264cba82d3a73dd
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 10:04:33 +0600

    Fix date parsing in createBookings function

--- src/modules/bookings/bookings.services.ts
+++ src/modules/bookings/bookings.services.ts
@@ -1,7 +1,7 @@
 const createBookings = async (payload: Record<string, unknown>) => {
     const {customer_id, vehicle_id, rent_start_date, rent_end_date} = payload;
 
-    const start_date = new Date(rent_start_date!);
+    const start_date = new Date(rent_start_date as string);
 };
 
 export const bookingServices = {
