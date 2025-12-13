8e2811a25e34a91fb7d03ab6b281cafa06021a5a
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 18:57:15 +0600

    Add vehicle booking logic to createBookings

--- src/modules/bookings/bookings.controller.ts
+++ src/modules/bookings/bookings.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { bookingServices } from "./bookings.services";
+import { vehicleServices } from "../vehicles/vehicles.services";
 
 const createBookings = async (req: Request, res: Response) => {
     try {
         const result = await bookingServices.createBookings(req.body);
-        console.log(result);
+        
+        await vehicleServices.(vehicle_id, "booked");
         
     } catch (err: any) {
         return res.status(400).json({
