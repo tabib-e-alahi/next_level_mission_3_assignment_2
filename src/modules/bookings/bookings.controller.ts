b1ed1f95359a2ec019f7dc07c5027b8f976472e2
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 09:45:33 +0600

    Implement booking controller and service.

---
+++ src/modules/bookings/bookings.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+const createBookings = async (req: Request, res: Response) => {};
+
+export const bookingController = {
+  createBookings,
+};
---
+++ src/modules/bookings/bookings.services.ts
@@ -0,0 +1,5 @@
+const createBookings = async (payload: Record<string, unknown>) => {};
+
+export const bookingServices = {
+  createBookings,
+};
