e0cfcae74664ead99cd6f8341b317d99c7bd9656
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 10:03:33 +0600

    Extract payload properties in bookings service

--- src/modules/bookings/bookings.services.ts
+++ src/modules/bookings/bookings.services.ts
@@ -1,5 +1,7 @@
 const createBookings = async (payload: Record<string, unknown>) => {
-    const {customer_id, vehicle_id, rent_start_date, rent_end_date}
+    const {customer_id, vehicle_id, rent_start_date, rent_end_date} = payload;
+
+    const start_date = new Date(rent_start_date!);
 };
 
 export const bookingServices = {
