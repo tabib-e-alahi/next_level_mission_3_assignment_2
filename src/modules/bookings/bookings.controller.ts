3403b52193ba853852710ea403c3ddf0146a2462
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 09:49:33 +0600

    Implement error handling in bookings controller.

--- src/modules/bookings/bookings.controller.ts
+++ src/modules/bookings/bookings.controller.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 
 const createBookings = async (req: Request, res: Response) => {
-    
+    try {
+        
+    } catch (err: any) {
+        return res.status(500).json()
+        
+    }
 };
 
 export const bookingController = {
