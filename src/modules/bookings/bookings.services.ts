a7cee1cc55aa7962cae91c3065fb4ab24b58e65f
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 10:02:33 +0600

    Remove unused createBookings function parameters

--- src/modules/bookings/bookings.services.ts
+++ src/modules/bookings/bookings.services.ts
@@ -1,5 +1,5 @@
 const createBookings = async (payload: Record<string, unknown>) => {
-    
+    const {customer_id, vehicle_id, rent_start_date, rent_end_date}
 };
 
 export const bookingServices = {
