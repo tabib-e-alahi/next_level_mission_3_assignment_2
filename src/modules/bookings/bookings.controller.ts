f0c6da1f2b0fd92fafc90c8bb570a2da6077abc2
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 19:04:15 +0600

    Update booking controller to use booking ID in vehicle update

--- src/modules/bookings/bookings.controller.ts
+++ src/modules/bookings/bookings.controller.ts
@@ -3,19 +3,20 @@ import { bookingServices } from "./bookings.services";
 import { vehicleServices } from "../vehicles/vehicles.services";
 
 const createBookings = async (req: Request, res: Response) => {
-    try {
-        const result = await bookingServices.createBookings(req.body);
-        
-        await vehicleServices.updateVehicleByID({vehicleId:vehicle_id, availability_status:"booked"});
-        
-    } catch (err: any) {
-        return res.status(400).json({
-            success: false,
-            message: "Booking failed.",
-            error: err.message
-        })
-        
-    }
+  try {
+    const result = await bookingServices.createBookings(req.body);
+
+    await vehicleServices.updateVehicleByID({
+      vehicleId: result.vehicle_id,
+      availability_status: "booked",
+    });
+  } catch (err: any) {
+    return res.status(400).json({
+      success: false,
+      message: "Booking failed.",
+      error: err.message,
+    });
+  }
 };
 
 export const bookingController = {
