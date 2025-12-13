adc0bd2d5a41745d514929373c1ed941f0fb7300
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-13 09:52:33 +0600

    Update bookings controller to use booking services

--- src/modules/bookings/bookings.controller.ts
+++ src/modules/bookings/bookings.controller.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from "express";
+import { bookingServices } from "./bookings.services";
 
 const createBookings = async (req: Request, res: Response) => {
     try {
+        const result = await bookingServices.createBookings(req.body);
         
     } catch (err: any) {
-        return res.status(500).json({
+        return res.status(400).json({
             success: false,
             message: "Booking failed.",
-            error: err
+            error: err.message;
         })
         
     }
