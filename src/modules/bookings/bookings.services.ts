e2f1950c82414e539e1b76709bc7770e8bb9da64
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-15 00:11:25 +0600

    Add booking authorization check

--- src/modules/bookings/bookings.services.ts
+++ src/modules/bookings/bookings.services.ts
@@ -91,6 +91,8 @@ const updateBookings = async (payload: Record<string, unknown>) => {
 
   const booking = booking_info.rows[0];
 
+  if
+
   if (userRole !== "admin" && booking.customer_id !== loggedInUserId) {
     throw new Error("You are not authorized to update this booking");
   }
