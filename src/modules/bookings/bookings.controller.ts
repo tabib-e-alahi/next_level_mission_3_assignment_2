eacbc6828a37009e1aa52b69b3226d48d8180051
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-14 19:21:01 +0600

    Add updateBookings method to bookings controller

--- src/modules/bookings/bookings.controller.ts
+++ src/modules/bookings/bookings.controller.ts
@@ -50,7 +50,7 @@ const getAllBookings = async (req: Request, res: Response) => {
 
 const updateBookings = async (req: Request, res: Response) => {
   try {
-    const result = 
+    const result = await bookingServices.updateBookings();
   } catch (err: any) {
     res.status(403).json({
       success: false,
@@ -63,4 +63,5 @@ const updateBookings = async (req: Request, res: Response) => {
 export const bookingController = {
   createBookings,
   getAllBookings,
+  updateBookings,
 };
