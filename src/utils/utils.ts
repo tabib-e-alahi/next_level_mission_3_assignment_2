cfb0878194ea4ae07bc7fa8d1a527c18e6f0b100
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-15 00:44:25 +0600

    Update autoReturnBookings to handle empty bookings list

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,21 +1,22 @@
 import { pool } from "../config/db";
 
 const autoReturnBookings = async () => {
-  
   const bookings_info = await pool.query(`
     SELECT id, vehicle_id
     FROM Bookings
     WHERE status = 'active'
   `);
 
-  if (bookings_info.rows.length === 0)
-  []
+  if (bookings_info.rows.length === 0) {
+    return;
+  }
+
+  
 
   for (const booking of expired.rows) {
-    await pool.query(
-      `UPDATE Bookings SET status='returned' WHERE id=$1`,
-      [booking.id]
-    );
+    await pool.query(`UPDATE Bookings SET status='returned' WHERE id=$1`, [
+      booking.id,
+    ]);
 
     await pool.query(
       `UPDATE Vehicles SET availability_status='available' WHERE id=$1`,
