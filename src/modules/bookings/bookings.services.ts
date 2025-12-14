900fb42e526be8f7a0bdb3232204d309ca7d0474
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-15 00:18:25 +0600

    Add vehicle info query to create bookings

--- src/modules/bookings/bookings.services.ts
+++ src/modules/bookings/bookings.services.ts
@@ -3,6 +3,8 @@ import { pool } from "../../config/db";
 const createBookings = async (payload: Record<string, unknown>) => {
   const { customer_id, vehicle_id, rent_start_date, rent_end_date } = payload;
 
+  const vehicle_info = await pool.query(`SELECT * FROM Vehicles `)
+
   const start_date = new Date(rent_start_date as string);
   const end_date = new Date(rent_end_date as string);
 
