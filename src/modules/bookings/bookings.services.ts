e17b53d94754718f91fac5c992922e8dea47e274
tabib-e-alahi <etabib.alahi@gmail.com>
2025-12-14 21:55:20 +0600

    Remove unused code in updateBookings function

--- src/modules/bookings/bookings.services.ts
+++ src/modules/bookings/bookings.services.ts
@@ -84,7 +84,7 @@ const updateBookings = async (payload: Record<string, unknown>) => {
     const booking_status = 'returned'
     const result = await pool.query(`UPDATE Bookings SET status=$1 RETURNING *`,[booking_status]);
     if(result.rows[0].status === 'returned'){
-      const 
+      
     }
   }
 };
